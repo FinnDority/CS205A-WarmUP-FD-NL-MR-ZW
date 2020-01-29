76e6d4b2790f93100985a5eaeabdae20cf3b741a
Finn Dority <FinnDority@users.noreply.github.com>
2020-01-29 09:02:08 -0500

    decreased the size of the largest lines

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,9 +1,7 @@
-
 import csv
 import sqlite3
 
-
-# load() creates the football database with two tables (teams and players). 
+# load() creates the football database with two tables (teams and players).
 # If the database already exists, it overwrites the two tables.
 def load():
 
@@ -15,26 +13,30 @@ def load():
     # Drop teams and player tables from database (if they exist) so they can be overwritten
     c.execute("drop table if exists teams")
     c.execute("drop table if exists players")
-    conn.commit()   #commit changes to database     
+    conn.commit()   #commit changes to database
     
     # create the teams table
-    c.execute("CREATE TABLE teams (team VARCHAR(32), location VARCHAR(32), stadium VARCHAR(32), capacity INTEGER, conference VARCHAR(32), region VARCHAR(32), PRIMARY KEY (team))")
+    c.execute("CREATE TABLE teams (team VARCHAR(32), location VARCHAR(32), stadium VARCHAR(32), capacity INTEGER,"
+              "conference VARCHAR(32), region VARCHAR(32), PRIMARY KEY (team))")
     
     # read data from the csv file into the table 
     with open('teams.csv', newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            c.execute("INSERT INTO teams (team, location, stadium, capacity, conference, region) VALUES (?, ?, ?, ?, ?, ?)", (row[0].lower(), row[1].lower(), row[2].lower(), int(row[3].replace(',', '')), row[4].lower(), row[5].lower(),))
+            c.execute("INSERT INTO teams (team, location, stadium, capacity, conference, region) VALUES (?, ?, ?, ?, ?, ?)",
+                      (row[0].lower(), row[1].lower(), row[2].lower(), int(row[3].replace(',', '')), row[4].lower(), row[5].lower(),))
     conn.commit()
     
 
     # do the same steps as above for the other table
-    c.execute("CREATE TABLE players (rank INTEGER, name VARCHAR(32), position VARCHAR(32), team VARCHAR(32), PRIMARY KEY (rank), FOREIGN KEY (team) REFERENCES teams (team))")
+    c.execute("CREATE TABLE players (rank INTEGER, name VARCHAR(32), position VARCHAR(32), team VARCHAR(32), PRIMARY KEY (rank),"
+              "FOREIGN KEY (team) REFERENCES teams (team))")
     
     with open('players.csv', newline='', encoding='utf-8-sig') as f:
         reader = csv.reader(f)
         for row in reader:
-            c.execute("INSERT INTO players (rank, name, position, team) VALUES (?, ?, ?, ?)", (int(row[0]), row[1].lower(), row[2].lower(), row[3].lower(),))
+            c.execute("INSERT INTO players (rank, name, position, team) VALUES (?, ?, ?, ?)",
+                      (int(row[0]), row[1].lower(), row[2].lower(), row[3].lower(),))
     
     conn.commit() # save the changes
     # close the connection
