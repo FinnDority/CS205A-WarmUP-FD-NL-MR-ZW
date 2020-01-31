1fdd50442226975059c97abf09ed23e5e784f9d9
zacharyward <zmward@uvm.edu>
2020-01-31 09:00:57 -0500

    splitting string into seperate words

--- HandleUserInput.py
+++ HandleUserInput.py
@@ -1,15 +1,19 @@
 def user_input():
 
-	print('Welcome to the NFL teams database\n'
+        print('Welcome to the NFL teams database\n'
 	        + 'You will be prompted below to enter information'
 	       + ' about players and teams.')
-	print('**********************************************')
+        print('**********************************************')
 
-        print()
+        print('Please enter your query below.')
+
+        user_input = input()
+        user_input = user_input.split()
+        print(user_input)
 
 # Main function to run the user input function.
 def main():
-	user_input()
 
+    user_input()
 
 main()
