b345420b427a4867f5e593e6417e9a2e6059b9a4
Michelle Rogals <mrogals1@uvm.edu>
2020-01-31 11:26:01 -0500

    added a help function

--- FrontEnd.py
+++ FrontEnd.py
@@ -42,6 +42,7 @@ def load():
     # close the connection
     conn.close()
 
+# example() is an example of how to access the database
 def example():
     # open a connection to the database
     db_file = "football.db"
@@ -51,5 +52,32 @@ def example():
     sql_command = "select p.player_name from teams t, players p where t.team_name = p.team_name and t.team_name = 'rams';"
     for row in c.execute(sql_command):
         print(row[0])
-      
+    
+    #close the connection
     conn.close()
+
+# help() prints information to help the user use the system.     
+def help():
+    print("User Commands                                                Description")
+    print("  help                                                        displays this menu")
+    print("  load data                                                   loads data to database")
+    print("  quit                                                        quits program")
+    print("  <column to display> <column to search> <value searched>     queries database")
+    print()
+    print("Example of valid query:")
+    print("  stadium team_name Bills")
+    print("      -prints the stadium name of the Bills team")
+    print()
+    print("Example of valid query:")
+    print("  team_name player_name 'Aaron Donald'")
+    print("      -prints the team that Aaron Donald belongs to")
+    print("      -NOTE: when there is a space in the value being searched,")
+    print("             quotation marks are required")
+    print()
+    print("Example of valid query:")
+    print("  team_name conference afc region east")
+    print("      -prints teams that are in the AFC and located in the east")
+    print()
+    print("Valid Column Names:")
+    print("  team_name, location, stadium, capacity, conference, division,") 
+    print("  rank, player_name, position")
