b586c819e39cd1ec96a342e49ac6dfe296c3f811
zacharyward <zmward@uvm.edu>
2020-02-04 23:30:53 -0500

    working on getting the proper words for the values

--- HandleUserInput.py
+++ HandleUserInput.py
@@ -13,55 +13,85 @@ region
 """
 def user_input():
 
+        key_words = []
+        value_words = []
+
         print('Welcome to the NFL teams database\n'
 	        + 'You will be prompted below to enter information'
 	       + ' about players and teams.')
-	print('**********************************************')
-
-	print()
-
-
-
-def split_input(query_string):
-    input_list = query_string.split()
-    for word in input_list:
-        if "\"" in word:
-            query_string.find("\"")
 
         print('Please enter your query below.')
 
         user_input = input()
         user_input = user_input.split()
 
+        for i in user_input:
 
+            if (i == 'rank'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
 
+            elif (i == 'player_name'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'position'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'team'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'team_name'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'location'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'stadium'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'capacity'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'confrence'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
+
+            elif (i == 'region'):
+                key_words.append(i)
+                print(user_input.index(i))
+                value_words.append(i)
+                print(value_words)
 
 # Main function to run the user input function.
 def main():
 
     user_input()
 
+
+# start of the main function
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def split_input(user_input):
-    input_list = user_input.split()
-    for word in input_list:
