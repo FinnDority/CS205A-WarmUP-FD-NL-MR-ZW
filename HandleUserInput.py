c58c0da0d2f0921d6b9038c09221aa86e3f128cc
zacharyward <zmward@uvm.edu>
2020-02-11 20:06:33 -0500

    Finished the two lists

--- HandleUserInput.py
+++ HandleUserInput.py
@@ -18,74 +18,59 @@ def user_input():
 
         print('Welcome to the NFL teams database\n'
 	        + 'You will be prompted below to enter information'
-	       + ' about players and teams.')
+	        + ' about players and teams.')
 
         print('Please enter your query below.')
 
         user_input = input()
         user_input = user_input.split()
 
+       """
+       This loop will just go through the list and add the key with its values to new lists
+       It's also easier to have the user input the players names without any spaces so then
+       the two lists have the key words and values synced at the same indexes.
+       """
         for i in user_input:
 
             if (i == 'rank'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'player_name'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'position'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'team'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'team_name'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'location'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'stadium'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'capacity'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'confrence'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
             elif (i == 'region'):
                 key_words.append(i)
-                print(user_input.index(i))
-                value_words.append(i)
-                print(value_words)
+                value_words.append(user_input[user_input.index(i) + 1])
 
 # Main function to run the user input function.
 def main():
